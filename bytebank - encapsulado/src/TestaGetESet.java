572fbdb3711f8bec63bd737b4c2cca994ae64917
Cleyfson <cleyfson12@outlook.com>
2022-03-28 14:57:03 -0300

    consolidando conceitos de POO em Java

---
+++ bytebank - encapsulado/src/TestaGetESet.java
@@ -0,0 +1,24 @@
+
+public class TestaGetESet {
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337, 24226);
+		
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		
+		paulo.setNome("Paulo Silveira");
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");
+		// agora em duas linhas 
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
+	}
+}
